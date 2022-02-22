9d05a0013357945b47eef948bcb23fdcf55ab51b
gonzahervalejo <gonzalohervalejo@hotmail.com>
2022-02-21 21:26:50 -0300

    Entrega 1

---
+++ assets/js/app.js
@@ -0,0 +1,20 @@
+const deportes = [{ nombre: "river", precio: 350},
+{nombre: "boca", precio:300 }];
+
+const soquetes = [{nombre: "mariposas", precio: 300 },
+{ nombre: "corazones", precio: 350},
+{ nombre: "lisos", precio: 300}];
+
+const invisibles = [{nombre: "rayados", precio:200},
+{nombre: "lisos", precio:200},
+{nombre: "old school", precio:250}];
+
+
+const menu = [deportes, soquetes, invisibles];
+console.log(menu);
+
+
+
+soquetes.forEach( producto =>  {producto.nombre
+
+console.log(producto.nombre) } );
